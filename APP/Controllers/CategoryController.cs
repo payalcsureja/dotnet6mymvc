36066078b95b74714598ccb6dd9eec96ca8eec03
payalcsureja <payalcsureja@gmail.com>
2022-01-24 00:21:56 -0500

    edit and delete feature

--- APP/Controllers/CategoryController.cs
+++ APP/Controllers/CategoryController.cs
@@ -52,6 +52,67 @@ namespace APP.Controllers
             return View(obj);
             
         }
+
+        [HttpGet("edit")]       
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id ==0){
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+            if(CategoryFromDb == null){
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+
+        [HttpPost("edit")]  
+        [ValidateAntiForgeryToken]     
+        public IActionResult Edit(Category obj)
+        {
+
+            if(obj.Name == obj.DisplayOrder.ToString()){
+                ModelState.AddModelError("CustomError", "Name and display order should not be same.");
+            }
+
+            if(ModelState.IsValid){
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+        }
+
+        [HttpGet("delete")]       
+        public IActionResult Delete(int? id)
+        {
+            if(id==null || id ==0){
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+            if(CategoryFromDb == null){
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+
+        [HttpPost("delete"), ActionName("Delete")]  
+        [ValidateAntiForgeryToken]     
+        public IActionResult DeletePost(int? id)
+        {
+ 
+            var obj = _db.Categories.Find(id);
+            if(obj==null){
+                return NotFound();
+            }            
+             
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            
+            
+        }
         
     }
 }
